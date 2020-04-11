57c608d76dca51c7654b7731982674ed399488b9
Javier <javelie@outlook.com>
2020-04-11 13:55:40 -0600

    pintView

--- travelBook/ViewController.swift
+++ travelBook/ViewController.swift
@@ -155,6 +155,8 @@ class ViewController: UIViewController {
             print("error")
         }
         
+        NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+        navigationController?.popViewController(animated: true)
         
     }
 }
@@ -177,5 +179,27 @@ extension ViewController: CLLocationManagerDelegate{
             
             mapView.setRegion(region, animated: true)
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "myAnnotation"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            pinView?.tintColor = UIColor.black
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            
+            pinView?.rightCalloutAccessoryView = button
+            
+        }else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
     }
 }
